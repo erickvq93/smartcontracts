1c6624cc64024730a7b36c4710acf57734706f65
Erick Queiroz <111603933+erickvq93@users.noreply.github.com>
2024-05-08 11:39:58 -0300

    Create IERC20.sol
    Interface ERC20 default

---
+++ tuneplay/IERC20.sol
@@ -0,0 +1,25 @@
+interface IERC20 {
+    function approve(
+        address owner,
+        address spender,
+        uint256 value,
+        bool emitEvent
+    ) external;
+
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+
+    function mint(address _to, uint256 _amount) external;
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function allowance(
+        address owner,
+        address spender
+    ) external view returns (uint256);
+}
